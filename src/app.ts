2e20ed8c0a8df2e26f6e2c098d8aca252c24c672
David Snyder <davidgraygs4@gmail.com>
2020-11-02 22:04:35 -0500

    init commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+// const middlewares = require("./middlewares");
+import {notFound, errorHandler} from "./middleware"
+// const api = require("./api");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+	});
+});
+
+app.use("/api/v1", api);
+
+app.use(notFound);
+app.use(errorHandler);
+
+exports = app;
---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import {Request, Response,  NextFunction} from "express"
+
+export function notFound(req: Request, res: Response, next: NextFunction): void {
+	res.status(404);
+	const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+	next(error);
+}
+
+/* eslint-disable no-unused-vars */
+export function errorHandler(err: Error, req: Request, res: Response): void {
+	/* eslint-enable no-unused-vars */
+	const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+	});
+}
+
